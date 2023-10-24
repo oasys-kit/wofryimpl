512af957c1dd319d43c978cbecdf61da80baf7c2
Manuel Sanchez del Rio <srio@esrf.eu>
2023-10-24 16:17:44 +0200

    updated version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except AttributeError:
 
 NAME = 'wofryimpl'
 
-VERSION = '1.0.21'
+VERSION = '1.0.22'
 ISRELEASED = True
 
 DESCRIPTION = 'WOFRY (Wave Optics FRamework in pYthon) standard implementation'
