f3706ca7a1295603dd4cb5f0b82a041b41872ebe
Manuel Sanchez del Rio <srio@esrf.eu>
2025-06-06 15:09:07 +0200

    added light_source_h5file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except AttributeError:
 
 NAME = 'wofryimpl'
 
-VERSION = '1.0.31'
+VERSION = '1.0.32'
 ISRELEASED = True
 
 DESCRIPTION = 'WOFRY (Wave Optics FRamework in pYthon) standard implementation'
